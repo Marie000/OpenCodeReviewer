2f8f39476d5bb444745bc3a4e32cf71de33df5ca
Marie Pelletier <marie.pelletier@gmail.com>
2016-12-15 20:33:16 -0500

    blah blah

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var PORT = process.env.port || 8080;
 var app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../build'));
+app.use(express.static(__dirname + '/../build/'));
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
