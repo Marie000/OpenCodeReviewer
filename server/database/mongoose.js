baa8fe683e1683543bc164b84f3c7b36c516ffc1
Marie Pelletier <marie.pelletier@gmail.com>
2016-12-15 19:56:46 -0500

    made changes to deploy to heroku

--- server/database/mongoose.js
+++ server/database/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require ('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/CodeReviewAppTest');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/CodeReviewAppTest');
 
 module.exports = {mongoose: mongoose};
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require ('express')
 var bodyParser = require('body-parser');
 var mongoose = require ('./database/mongoose.js');
 
-var PORT = 8080;
+var PORT = process.env.port || 8080;
 var app = express();
 
 app.use(bodyParser.json());
