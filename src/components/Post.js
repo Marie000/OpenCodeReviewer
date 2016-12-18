066c28bddeab31908de23c83f9a98c60ed537370
Dana Nica <dana.mn@gmail.com>
2016-12-17 20:48:39 -0500

    Send comments to the db and display them in list

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,28 +10,28 @@ import 'codemirror/mode/htmlmixed/htmlmixed';
 import 'codemirror/addon/edit/matchbrackets';
 import 'codemirror/addon/edit/closebrackets';
 
-
 export default class Post extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-    	id: this.props.params.postId
+    	id: this.props.params.postId,
+      comments:[]
     }
   }
 
   getPostData(){
   	var scope = this;
+
   	var data = HTTP.get('/documents/'+this.state.id)
     .then(function(data){
         scope.setState({
           id: data._id,
           title:data.title,
-          author:data.author,
+          author:data._author.user_name,
           text: data.text,
           tags: data.tags,
-          date_submited:data.date_submitted,
           comments:data.comments
-        })
+        });
       })
   }
 
@@ -39,44 +39,37 @@ export default class Post extends Component {
     this.getPostData()
     } 
 
-
-   handleSubmit(){
-   		this.getPostData();
-    }
+  reloadPage(){
+    this.getPostData();
+  }
     
   render() {
-  	var displayedComment='';
-  	var commentsList = []
-  	var existingComments = this.state.comments;
-  	if (existingComments ){
-  		if(existingComments.length==0){
-  			displayedComment = '';
-  		} else if (existingComments.length>0){
-  			existingComments.forEach(function(comment){  				
-  				commentsList.push(comment.text)
-  			})
-  			displayedComment = <p>Comment:{commentsList.join(', ')}</p>
-  		}
-  	}
 
     var options = {
       lineNumbers: true,
       mode: 'javascript',
       matchBrackets: true,
       closebrackets: true
-
     }
   	
     return (
       <div>
         <h2>Title: {this.state.title}</h2>
+        ID: {this.state.id}
         <p>Tags: {this.state.tags}</p>
-        <p>Text: <CodeMirror value={this.state.text} options={options} /></p>
-        {displayedComment}
-   		
-        <p> Posted on: {this.state.date_submited} by {this.state.author} </p>
-        <form onSubmit = {this.handleSubmit.bind(this)}>
-        	<PostComment id={this.state.id}></PostComment>
+        <div>Text: <CodeMirror value={this.state.text} options={options} /></div>
+        <p> Comments:  </p>
+        <ul>
+          {this.state.comments.map(comment => { return (
+            <li> {comment.text} <br/>
+             Comment by {comment._author}
+            </li>
+          )}
+        )}       
+   		 </ul>
+        <p> Posted by {this.state.author} </p>
+        <form >
+        	<PostComment id={this.state.id} reload={this.reloadPage.bind(this)}></PostComment>
         </form>
       </div>
 
--- src/components/PostComment.js
+++ src/components/PostComment.js
@@ -1,65 +1,47 @@
 import React, { Component } from 'react';
-import Moment from 'moment';
 import HTTP from '../services/httpservice';
-import randomstring from 'randomstring';
-
-/* <button onClick={this.handleSubmit.bind(this)}>Submit comment</button>*/
 
 export default class PostComment extends Component {
+
   constructor(props) {
     super(props);
     this.state = { 
-      postId: this.props.id
+      "_document_id": this.props.id,
+      "is_general":"true",
+      "position":"null",
+      text:""
     }
   }
 
-
   handleChange (e) {
-    var comment = e.target.value;
-    
+    var comment = e.target.value;   
     this.setState({
     	text:comment,    	
     });
   }
 
   handleSubmit(e) {
-    //e.preventDefault();
+    e.preventDefault();
+    var data = this.state;
+    HTTP.post('/comments/', data);
 
-    var id =  randomstring.generate(7);
-    var commentObj = {
-    	id: id,
-    	text: this.state.text,
-    	date_submitted:Moment(new Date()).locale('en').format('MMM DD YYYY, HH:mm')
-    }
-    var data = {
-    	id:this.state.postId,
-    	comment: commentObj
-    }
-    console.log(data)
-
-    var request = new XMLHttpRequest();
-    request.open('POST', 'http://localhost:6060/posts', true);
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    request.send('data='+JSON.stringify(data));
-
+    window.setTimeout(function () {  
+       this.props.reload();
+       this.setState({
+          text:""
+       })
+    }.bind(this), 800); 
 	}
-
-
 
   render(){
   	return(
   		<div>
-
           <label> Your comment: </label>
-          <input type="text" name="comment" value={this.state.text} 
-                             onChange={this.handleChange.bind(this)}>
-          </input>  <br/>
-         
-          <input type="submit" value="Submit" ></input>
-         </div>
-       
+          <textarea name="comment" value={this.state.text} onChange={this.handleChange.bind(this)} />
+          <br/>      
+          <button onClick={this.handleSubmit.bind(this)} >Submit your comment</button>
+         </div>       
   	)
   }
-
  }
 
