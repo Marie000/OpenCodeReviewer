e4e4e20184bef4fa82860cdf57ff5e4695c63a72
Dana Nica <dana.mn@gmail.com>
2017-01-08 00:11:41 -0500

    delete http import

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import HTTP from '../services/httpservice';
 import CodeMirror from 'react-codemirror';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/javascript/javascript';
