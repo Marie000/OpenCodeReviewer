78c4ecb1e7d4efe2e87062021f6be61809db1583
Marie Pelletier <marie.pelletier@gmail.com>
2017-01-23 19:40:28 -0500

    added the about page with filler text.

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+const About = ()=>{
+  return <div>
+    <h1>About CheckMyCode</h1>
+    <p>Lorem ipsum dolor sit amet, affert epicurei nec eu.
+      Pri malis theophrastus te, sea eu nisl vivendum, ius te posse mnesarchum necessitatibus.
+      Ad quo congue graecis accommodare, at tibique constituto quo. Ne viris audiam recusabo vim. Id eam fugit iracundia.</p>
+    <h2>Our Code of Conduct</h2>
+    <p>Lorem ipsum dolor sit amet, cu vis elitr nonumes definitionem, delicata facilisis ut vim. Ne feugait expetendis philosophia vim. His eu zril fastidii molestie, mollis noluisse at sea. Qui ex quidam malorum inimicus, dolor urbanitas eam an. Vitae omnesque ut usu.
+    </p>
+      <p>
+      Hinc accumsan delicatissimi in mea, adhuc constituto nam ex. Purto oratio meliore an sea. Idque tantas erroribus te pro. Qui ut munere vivendo appellantur, mel rebum expetenda patrioque ut.
+</p>
+      <p>
+      Id pri primis expetendis, te pro iusto accommodare, ludus equidem pri ad. Ad vis cetero tamquam. Eum ad modus legere scripserit. Id eam quaeque dolorum sententiae. Pri ex graecis apeirian persequeris, ne pro veri falli omittam. Ea nec natum eruditi atomorum.
+</p><p>
+      Summo choro iisque eum ut. Te solet diceret delicata usu, natum veniam forensibus mei ut, et nulla iisque pri. Falli vocibus prodesset an duo, ancillae suavitate eu mea. Ei probo volumus assentior sea.</p>
+  </div>
+}
+
+module.exports = About;
--- src/components/App.js
+++ src/components/App.js
@@ -51,12 +51,12 @@ class App extends Component {
 
     var buttons;
       if (!localStorage.user_name){
-        buttons = <ul className='menu'><a className='link' to="/signin" ><li className='inline-blk button-darkgreen'>About</li></a>
+        buttons = <ul className='menu'><Link className='link' to="/about" ><li className='inline-blk button-darkgreen'>About</li></Link>
         <Link className='link' to="/signin" ><li className=' mrgLeft10 inline-blk  button-darkgreen'>Sign In</li></Link>
         <Link className='link' to="/login" ><li className='mrgLeft10 inline-blk  button-darkgreen'>Log In</li></Link>
         </ul>
       } else {
-        buttons = <ul className='menu'><a className='link' to="/signin" ><li className='inline-blk button-darkgreen'>About</li></a>
+        buttons = <ul className='menu'><Link className='link' to="/about" ><li className='inline-blk button-darkgreen'>About</li></Link>
         <Link className='link' to={'/profile/'+localStorage.user_id}><li className='button-darkgreen inline-blk mrgLeft10'>Profile</li></Link>
         <Link className='link' to="/dashboard" ><li className='mrgLeft10 button-darkgreen inline-blk' onClick={this.logOutUser.bind(this)}>Log Out</li></Link></ul>
       }
