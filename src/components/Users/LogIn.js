4151d1fee812fc443640ca9f88d6bae867b68ae6
Marie Pelletier <marie.pelletier@gmail.com>
2017-02-18 14:05:37 -0500

    repo link in meta tag, link to register in login, logout redirects to dashboard and number of comments and date in post list

--- src/components/Users/LogIn.js
+++ src/components/Users/LogIn.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { LoginForm } from 'react-stormpath';
+import { Link } from 'react-router';
 
 export default function LogIn () {
-  return <LoginForm />
+  return <div>
+      <LoginForm />
+    <p className="registerLink">New User? <Link to="/register">Register Here</Link></p>
+    </div>
 }
