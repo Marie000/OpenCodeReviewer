70d23a68933c4a50b59c922cab423949fe260f22
Marie Pelletier <marie.pelletier@gmail.com>
2017-02-20 21:55:22 -0500

    refactored post to have separate postContent component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -8,6 +8,7 @@ import {Authenticated} from 'react-stormpath';
 
 import CommentForm from './CommentForm';
 import PostCommentList from './PostCommentList';
+import PostContent from './PostContent';
 
 import CodeMirror from 'react-codemirror';
 import 'codemirror/lib/codemirror.css';
@@ -51,15 +52,6 @@ export default class Post extends Component {
   reloadPage(){
     this.getPostData();
     this.getInlineComments();
-  }
-
-  handleChange (e) {
-    var comment = e.target.value;   
-    this.setState({
-      currentComment:{
-        text:comment
-      }  
-    });
   }
 
   getPostData(){
@@ -120,90 +112,6 @@ export default class Post extends Component {
     })
   }
 
-  addWidget(){
-    var container = document.createElement("div");
-    container.id = "currentCommentDiv"
-    container.innerHTML = "<textarea class='inline-comment inline-comment-text' placeholder='Type your comment here' id = 'currentComment'></textarea>" ;
-
-    var codemir = this.refs.codemirror.getCodeMirror();
-
-   // var line = codemir.getCursor("to").line;
-    var lastLine = codemir.getCursor("end").line;
-    var lastch = codemir.getCursor("end").ch;
-    var firstch = codemir.getCursor("first").ch;
-    var firstLine = codemir.getCursor('start').line;
-
-    var currentWidget = codemir.addLineWidget(lastLine, container, {
-      coverGutter: false
-    });
-
-    this.setState({
-      currentComment:{
-          widget: currentWidget,
-          firstLine: firstLine,
-          lastLine:lastLine
-//        text: document.getElementById('comment').value
-      }
-    })
-  }
-
-  cancelWidget(){
-   // var element = document.getElementById("currentCommentDiv");
-
-    var codemir = this.refs.codemirror.getCodeMirror();
-    this.state.currentComment.widget.clear();
-    this.setState({currentComment:{widget:null}})
-  }
-
-  saveComment(){
-    var currentCommentText = document.getElementById('currentComment').value;
-
-      var dataToSend={
-        position: 
-          {firstLine:this.state.currentComment.firstLine, 
-            lastLine:this.state.currentComment.lastLine},
-        text: currentCommentText,
-        is_general: false,
-        _document_id: this.state.id      
-      }
-
-      this.state.currentComment.widget.clear();
-
-      axios.post('/api/comments/', dataToSend)
-        .then(()=>{
-          dataToSend._author = {}
-          dataToSend._author.user_name = localStorage.user_name;
-          dataToSend.createdAt = new Date();
-          this.setState({
-            currentComment: {
-              firstLine: null,
-              lastLine: null,
-              widget: null
-            },
-            inlineComments:true
-          })
-          this.displayInlineComment(dataToSend, dataToSend.position)
-        })
-  }
-    
-
-  hideComments(){
-    $('.inline-comment').hide();
-    this.setState({commentsHidden : true})
-  }
-
-  showComments(){
-    this.setState({commentsHidden : false})
-
-    window.setTimeout(function(){
-     $('.inline-comment').show();
-     $(".selection").parents('.CodeMirror-line').addClass('selection-background');
-    }.bind(this), 50);
-  }
-
-
-
-
   render() {
     var codemirrorMode="text";
     languageList.forEach((language)=>{
@@ -238,37 +146,12 @@ export default class Post extends Component {
            </div> 
         </div>
 
-        {this.state.loggedIn ?
-          <div className="clearfix mrgBtm20 font18rem">
-          {this.state.currentComment.widget ? 
-            <div>
-            <button className='button-darkgreen-small link mrgRight10' onClick={this.saveComment.bind(this)} > Save comment </button>
-             <button className='button-darkgreen-small link mrgRight10' onClick={this.cancelWidget.bind(this)} > Cancel </button>
-             </div>
-          :
-            <div>
-            To post an inline comment, select the part of code that you wish to comment and press 
-             <button className='button-darkgreen-small link  mrgLeft10 mrgRight10' onClick={this.addWidget.bind(this)} > Add inline comment </button>
-            </div>
-          }
-          </div>
-        : null }      
-        </div>
+       <PostContent text={this.state.text} 
+                    reload={this.reloadPage.bind(this)}
+                    id={this.state.id}
+                    comments={this.state.comments}
+       />
 
-        <div className="codemirror-wrapper"><CodeMirror value={this.state.text} ref="codemirror" options={options} /></div>
-   
-
-      {this.state.inlineComments ? 
-        <div> {this.state.commentsHidden ? 
-        <button className='button-darkgreen-small link inline-blk pull-right mrgTop10 mrgLeft10 mrgRight10' onClick={this.showComments.bind(this)} > Show inline comments </button>
-        :
-        <button className='button-darkgreen-small link inline-blk pull-right mrgTop10 mrgLeft10 mrgRight10' onClick={this.hideComments.bind(this)} > Hide inline comments </button>  
-        }  </div> : null
-      } 
-
-         <div className='post-title mrgBtm20 inline-blk clearfix '> Posted by <span className='red'> <Link className='link' to={'/profile/'+this.state.author_id}>{this.state.author}</Link> </span> on {moment(this.state.postCreationDate).format("MMMM Do YYYY, h:mm:ss a")} </div>
-        
-        
         <PostCommentList comments={this.state.comments} reload={this.reloadPage.bind(this)} />
         
         <Authenticated>
@@ -278,6 +161,7 @@ export default class Post extends Component {
          </Authenticated>
       
       </div>
+        </div>
 
     )
   }
