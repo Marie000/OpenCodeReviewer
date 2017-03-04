8e47f7f7f85d5cb7e09e65e78b8d41f7614e54dd
Marie Pelletier <marie.pelletier@gmail.com>
2017-03-04 09:03:27 -0500

    /

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -10,7 +10,7 @@ export default class AuthService {
     // Configure Auth0
     this.lock = new Auth0Lock(clientId, domain, {
       auth: {
-        redirectUrl: '/',
+        redirectUrl: host+'/',
         responseType: 'token'
       }
     })
