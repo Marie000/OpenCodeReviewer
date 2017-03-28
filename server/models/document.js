0517296338119551e3680f3b00f69e5c889b096b
Antsa Randriamihaja <arandriamihaja@gmail.com>
2017-03-28 11:43:04 -0400

    url field added to document model

--- server/models/document.js
+++ server/models/document.js
@@ -1,52 +1,57 @@
 var mongoose = require ('mongoose');
 var mongoosePaginate = require('mongoose-paginate');
 var IdObject = mongoose.Schema.Types.ObjectId;
+require('mongoose-type-url');
 
 var CodeDocSchema = mongoose.Schema({
-  _author:{
-    type: IdObject,
-    required: true,
-    ref: 'User'
-  },
-  title:{
-    type: String,
-    required: true,
-    trim: true,
-    minlength:1
-  },
-  description:{
-    type: String,
-    trim: true
-  },
-  tags:[{
-    type: String,
-    trim: true
-  }],
-  editedAt: {
-    type: Date
-  },
-  commentedAt: {
-    type: Date
-  },
-  open_for_review: {
-    type: Boolean,
-    default: true
-  },
-  text: {
-    type: String
-  },
-  comments: [{
-    type: IdObject,
-    ref: 'Comment'
-  }],
-  language: {
-    type: String,
-    default:'text'
-  },
-  multi_files: {
-    type:Boolean,
-    default:false
-  }
+    _author:{
+        type: IdObject,
+        required: true,
+        ref: 'User'
+    },
+    title:{
+        type: String,
+        required: true,
+        trim: true,
+        minlength:1
+    },
+    description:{
+        type: String,
+        trim: true
+    },
+    tags:[{
+        type: String,
+        trim: true
+    }],
+    editedAt: {
+        type: Date
+    },
+    commentedAt: {
+        type: Date
+    },
+    open_for_review: {
+        type: Boolean,
+        default: true
+    },
+    text: {
+        type: String
+    },
+    comments: [{
+        type: IdObject,
+        ref: 'Comment'
+    }],
+    language: {
+        type: String,
+        default:'text'
+    },
+    multi_files: {
+        type:Boolean,
+        default:false
+    },
+    url: {
+        work: mongoose.SchemaTypes.Url,
+        profile: mongoose.SchemaTypes.Url
+    }
 });
 
 CodeDocSchema.set('timestamps',true);
